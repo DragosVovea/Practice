949ab6f1ceb08f81c7d9345658e69a3e08518440
Vovea Dragos <vovea.dragos@yahoo.com>
2025-01-14 19:44:14 +0200

    First Commit

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from repository.studentrepository import Repository
 from service.studentservice import Service
 from ui.console import Interface
 
-repository=Repository('C:\Apps\Pycharm\Practice\students')
-service=Service(repository)
-interface=Interface(service)
-interface.menu()
+def main():
+    repository=Repository('C:\Apps\Pycharm\Practice\students')
+    service=Service(repository)
+    interface=Interface(service)
+    interface.menu()
+
+if __name__ == '__main__':
+    main()
